518df7af438e67ff09cc54806dae08486722e811
me <me@local.host>
2026-07-31 17:36:55 +0700

    Revert "buffer size?"
    This reverts commit cb785165fd027a67a8394d28ebec045babb832fa.

--- easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/tools/ClientStream.java
+++ easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/tools/ClientStream.java
@@ -101,24 +101,16 @@ public class ClientStream {
       for (int i = 0; i < reTry; i++) {
         try {
           if (!mainConn) {
-	    mainSocket = new Socket();
-
-	    mainSocket.setTcpNoDelay(true);
-	    mainSocket.setReceiveBufferSize(1024 * 32);
-	    mainSocket.setSendBufferSize(1024 * 32);
-
-	    mainSocket.connect(inetSocketAddress, timeoutDelay / 2);
-	    mainConn = true;
-	}
-
-	videoSocket = new Socket();
-
-	videoSocket.setTcpNoDelay(true);
-	videoSocket.setReceiveBufferSize(1024 * 64);
-	videoSocket.setSendBufferSize(1024 * 64);
-
-	videoSocket.connect(inetSocketAddress, timeoutDelay / 2);
-
+            mainSocket = new Socket();
+            mainSocket.connect(inetSocketAddress, timeoutDelay / 2);
+            mainConn = true;
+          }
+          videoSocket = new Socket();
+          videoSocket.connect(inetSocketAddress, timeoutDelay / 2);
+          mainOutputStream = mainSocket.getOutputStream();
+          mainDataInputStream = new DataInputStream(mainSocket.getInputStream());
+          videoDataInputStream = new DataInputStream(videoSocket.getInputStream());
+          connectDirect = true;
           return;
         } catch (Exception ignored) {
           if (mainSocket != null) mainSocket.close();
