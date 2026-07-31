cb785165fd027a67a8394d28ebec045babb832fa
me <me@local.host>
2026-07-31 17:33:13 +0700

    buffer size?

--- easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/tools/ClientStream.java
+++ easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/tools/ClientStream.java
@@ -101,16 +101,24 @@ public class ClientStream {
       for (int i = 0; i < reTry; i++) {
         try {
           if (!mainConn) {
-            mainSocket = new Socket();
-            mainSocket.connect(inetSocketAddress, timeoutDelay / 2);
-            mainConn = true;
-          }
-          videoSocket = new Socket();
-          videoSocket.connect(inetSocketAddress, timeoutDelay / 2);
-          mainOutputStream = mainSocket.getOutputStream();
-          mainDataInputStream = new DataInputStream(mainSocket.getInputStream());
-          videoDataInputStream = new DataInputStream(videoSocket.getInputStream());
-          connectDirect = true;
+	    mainSocket = new Socket();
+
+	    mainSocket.setTcpNoDelay(true);
+	    mainSocket.setReceiveBufferSize(1024 * 32);
+	    mainSocket.setSendBufferSize(1024 * 32);
+
+	    mainSocket.connect(inetSocketAddress, timeoutDelay / 2);
+	    mainConn = true;
+	}
+
+	videoSocket = new Socket();
+
+	videoSocket.setTcpNoDelay(true);
+	videoSocket.setReceiveBufferSize(1024 * 64);
+	videoSocket.setSendBufferSize(1024 * 64);
+
+	videoSocket.connect(inetSocketAddress, timeoutDelay / 2);
+
           return;
         } catch (Exception ignored) {
           if (mainSocket != null) mainSocket.close();
