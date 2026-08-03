6220b3f553e8979f9e1f79277746958f26db9064
me <me@local.host>
2026-08-03 19:22:41 +0700

    Revert "make the stream render immediately"
    This reverts commit 99bfe088ab96be46dcc7ce9b5bb182a174cab5a4.
    reson is it just makes everything not smooth so i just take the old one

--- easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/decode/VideoDecode.java
+++ easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/decode/VideoDecode.java
@@ -25,7 +25,7 @@ public class VideoDecode {
     @Override
     public void onOutputBufferAvailable(@NonNull MediaCodec mediaCodec, int outIndex, @NonNull MediaCodec.BufferInfo bufferInfo) {
       try {
-        mediaCodec.releaseOutputBuffer(outIndex, true);
+        mediaCodec.releaseOutputBuffer(outIndex, bufferInfo.presentationTimeUs);
       } catch (IllegalStateException ignored) {
       }
     }
