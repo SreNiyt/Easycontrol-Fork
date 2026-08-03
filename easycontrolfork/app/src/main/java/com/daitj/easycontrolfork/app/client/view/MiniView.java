958e8bca8e7dfc9f54ae0cee48628fa9106ff338
me <me@local.host>
2026-08-04 04:48:47 +0700

    Revert "yea fixing the fling thing"
    This reverts commit 8eb838f5cc4f349c834a9560d2731182379c4e72.

--- easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/view/MiniView.java
+++ easycontrolfork/app/src/main/java/com/daitj/easycontrolfork/app/client/view/MiniView.java
@@ -81,57 +81,64 @@ private void animateMiniViewTo(int targetX, int targetY) {
   animator.start();
 }
 
-private void flingMiniView(float velocityX, float velocityY) {
+private void flingMiniView(float velocityX) {
   int screenWidth = AppData.applicationContext
       .getResources()
       .getDisplayMetrics()
       .widthPixels;
 
-  int screenHeight = AppData.applicationContext
-      .getResources()
-      .getDisplayMetrics()
-      .heightPixels;
-
   int viewWidth = miniView.getRoot().getWidth();
-  int viewHeight = miniView.getRoot().getHeight();
 
-  final float[] vx = {velocityX};
-  final float[] vy = {velocityY};
+  int currentX = miniViewParams.x;
 
   final float friction = 0.92f;
 
+  float flingDistance = velocityX * 0.35f;
+
+  int targetX;
+
+  if (velocityX < 0) {
+    targetX = 0;
+  } else {
+    targetX = screenWidth - viewWidth;
+  }
+
+  if (Math.abs(velocityX) < 100) {
+    targetX = currentX < screenWidth / 2
+        ? 0
+        : screenWidth - viewWidth;
+
+    animateMiniViewTo(
+        targetX,
+        miniViewParams.y
+    );
+
+    return;
+  }
+
+  final float[] currentVelocity = {velocityX};
+
   ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
 
-  animator.setDuration(2000);
+  animator.setDuration(1500);
 
   animator.addUpdateListener(animation -> {
+    float progress = (float) animation.getAnimatedValue();
 
-    vx[0] *= friction;
-    vy[0] *= friction;
+    currentVelocity[0] *= friction;
 
-    miniViewParams.x += (int) (vx[0] * 0.016f);
-    miniViewParams.y += (int) (vy[0] * 0.016f);
-
-    boolean hitEdge = false;
+    miniViewParams.x +=
+        (int) (currentVelocity[0] * 0.016f);
 
     if (miniViewParams.x <= 0) {
       miniViewParams.x = 0;
-      hitEdge = true;
-    }
+      currentVelocity[0] = 0;
+      animation.cancel();
 
-    else if (miniViewParams.x >= screenWidth - viewWidth) {
+    } else if (miniViewParams.x >= screenWidth - viewWidth) {
       miniViewParams.x = screenWidth - viewWidth;
-      hitEdge = true;
-    }
-
-    if (miniViewParams.y <= 0) {
-      miniViewParams.y = 0;
-      hitEdge = true;
-    }
-
-    else if (miniViewParams.y >= screenHeight - viewHeight) {
-      miniViewParams.y = screenHeight - viewHeight;
-      hitEdge = true;
+      currentVelocity[0] = 0;
+      animation.cancel();
     }
 
     device.miniY = miniViewParams.y;
@@ -141,26 +148,15 @@ private void flingMiniView(float velocityX, float velocityY) {
         miniViewParams
     );
 
-    if (hitEdge) {
-      animator.cancel();
-      return;
-    }
+    if (Math.abs(currentVelocity[0]) < 10) {
+      animation.cancel();
 
-    if (Math.abs(vx[0]) < 10
-        && Math.abs(vy[0]) < 10) {
-
-      animator.cancel();
-
-      int targetX;
-
-      if (miniViewParams.x < screenWidth / 2) {
-        targetX = 0;
-      } else {
-        targetX = screenWidth - viewWidth;
-      }
+      int finalX = miniViewParams.x < screenWidth / 2
+          ? 0
+          : screenWidth - viewWidth;
 
       animateMiniViewTo(
-          targetX,
+          finalX,
           miniViewParams.y
       );
     }
@@ -284,13 +280,10 @@ private void setBarListener() {
         velocityTracker[0].computeCurrentVelocity(1000);
 
         float velocityX = velocityTracker[0].getXVelocity();
-        float velocityY = velocityTracker[0].getYVelocity();
 
         if (isDragging[0]) {
-	  flingMiniView(
-	      velocityX,
-	      velocityY
-	  );
+
+          flingMiniView(velocityX);
 
         } else {
 
